d40dca8204aebc8f9735547d3caee129ae615570
maxspangler <maxspangler@gmail.com>
2020-12-25 20:48:05 -0500

    updated function 2

--- assets/index.js
+++ assets/index.js
@@ -55,7 +55,7 @@ function sendMessages(form) {
 
   clearForm(form);
 
-  fetch('../functions/send-messages', {
+  fetch('../functions/send-messages.js', {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
