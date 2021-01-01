223dcd870f734f37c6ac808c2fb15e4fd5cfb623
maxspangler <maxspangler@gmail.com>
2021-01-01 11:39:53 -0500

    logging the body

--- functions/send-messages.js
+++ functions/send-messages.js
@@ -1,5 +1,5 @@
 exports.handler = function (context, event, callback) {
-  console.log(event.body)
+  console.log(event)
   const phoneNumbers = event.recipients.split(',').map((x) => x.trim());
   const message = event.message;
   const passcode = event.passcode;
